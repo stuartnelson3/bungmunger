26931154b90b2b3bbcf89e88273f0408b9aa2ccb
stuart nelson <stuartnelson3@gmail.com>
2020-04-24 15:49:55 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,19 @@ fn main() {
     let range = match excel.worksheet_range_at(0) {
         Some(Ok(r)) => {
             println!("worksheet");
-            match r.rows().enumerate().find(|&row| 
-                match row.1[0].get_string() {
+            match r
+                .rows()
+                .enumerate()
+                .find(|&row| match row.1[0].get_string() {
                     Some(s) => s.trim() == "frame",
-                    None => false
-                }
-            
-            ) 
-            {
+                    None => false,
+                }) {
                 Some((idx, _)) => {
-                    let start = ((idx+1) as u32,0);
+                    let start = ((idx + 1) as u32, 0);
                     let end = r.end().unwrap();
-                    r.range(start,end)
+                    r.range(start, end)
                 }
-                None => panic!("not found!")
+                None => panic!("not found!"),
             }
         }
         Some(Err(e)) => {
@@ -54,13 +53,11 @@ fn main() {
         let t = row[0].get_float().unwrap() as i64;
         if t == 0 {
             // new particle, add to vec
-            particles.push(Particle{
-                detections: vec![]
-            });
+            particles.push(Particle { detections: vec![] });
         }
         // append to the last particle in our vec
         let len = particles.len();
-        let particle = &mut particles[len-1];
+        let particle = &mut particles[len - 1];
 
         particle.detections.push(Detection {
             t: t,
@@ -70,5 +67,8 @@ fn main() {
         });
     });
 
-    println!("{:?}",particles);
+    let track = Track {
+        particles: particles,
+    };
+    println!("{:?}", track);
 }
