bf0877fec10328fe4a53aaced4f6a149d48ae8c4
stuart nelson <stuartnelson3@gmail.com>
2020-04-24 15:45:28 +0200

    parsing the file and outputting particles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+#[derive(Debug)]
 struct Track {
-    particles: Vec<Particle>
+    particles: Vec<Particle>,
 }
 
+#[derive(Debug)]
 struct Particle {
-    detection: Vec<Detection>
+    detections: Vec<Detection>,
 }
 
 #[derive(Debug)]
@@ -11,55 +13,62 @@ struct Detection {
     t: i64,
     x: f64,
     y: f64,
-    z: f64
+    z: f64,
 }
 
 fn main() {
     use calamine::{open_workbook, Reader, Xlsx};
 
-    let mut i = 0;
-    println!("hello world");
-
+    println!("opening");
     let mut excel: Xlsx<_> = open_workbook("example.xlsx").unwrap();
-    match excel.worksheet_range_at(0) {
+    println!("opened");
+    let range = match excel.worksheet_range_at(0) {
         Some(Ok(r)) => {
-            let mut data_found = false;
-        for row in r.rows() {
-            // something is up with scoping, not finding this data_found
-            // business.
-            if i > 10 {
-                return;
-            }
-            match row[0].get_string() {
-                Some(s) => {
-                    if s == "frame" {
-                        data_found = true;
-                        continue
-                    }
+            println!("worksheet");
+            match r.rows().enumerate().find(|&row| 
+                match row.1[0].get_string() {
+                    Some(s) => s.trim() == "frame",
+                    None => false
                 }
-                None => continue
+            
+            ) 
+            {
+                Some((idx, _)) => {
+                    let start = ((idx+1) as u32,0);
+                    let end = r.end().unwrap();
+                    r.range(start,end)
+                }
+                None => panic!("not found!")
             }
-            if !data_found {
-                continue
-            }
-            if i > 10 {
-                return
-            }
-            let d = Detection {
-                t : row[0].get_int().unwrap(),
-                x: row[1].get_float().unwrap(),
-                y: row[2].get_float().unwrap(),
-                z: 0.0
-            };
-            println!("{:?}", d);
-            i += 1
-        }
         }
         Some(Err(e)) => {
-            println!("{}",e);
+            panic!("error: {}", e);
         }
         None => {
-            println!("none!");
+            panic!("none!");
         }
-    }
+    };
+
+    let mut particles: Vec<Particle> = vec![];
+    range.rows().for_each(|row| {
+        let t = row[0].get_float().unwrap() as i64;
+        if t == 0 {
+            // new particle, add to vec
+            particles.push(Particle{
+                detections: vec![]
+            });
+        }
+        // append to the last particle in our vec
+        let len = particles.len();
+        let particle = &mut particles[len-1];
+
+        particle.detections.push(Detection {
+            t: t,
+            x: row[1].get_float().unwrap(),
+            y: row[2].get_float().unwrap(),
+            z: 0.0,
+        });
+    });
+
+    println!("{:?}",particles);
 }
