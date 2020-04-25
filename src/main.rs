9216a5248f2585cc2674acc147db9271357ec378
stuart nelson <stuartnelson3@gmail.com>
2020-04-25 12:49:10 +0200

    output xml

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+extern crate calamine;
+
+use calamine::{open_workbook, Reader, Xlsx};
+
 #[derive(Debug)]
 struct Track {
     particles: Vec<Particle>,
 }
 
+impl Track {
+    fn xml(&self) -> String {
+        // make sure this is strings joined by newlines.
+        let particles = self
+            .particles
+            .iter()
+            .map(|p| format!("  {}", p.xml()))
+            .collect::<Vec<String>>()
+            .join("\n");
+        format!(
+            "<Tracks nTracks='{}' spaceUnits='pixel' frameInterval='1.0' timeUnits='frame' generationDateTime='Fr, 13 Mai 2016 21:49:25' from='TrackMate v2.8.1'>\n{}\n</Tracks>",
+            self.particles.len(),
+            particles
+        )
+    }
+}
+
 #[derive(Debug)]
 struct Particle {
     detections: Vec<Detection>,
+}
+
+impl Particle {
+    fn xml(&self) -> String {
+        let detections = self
+            .detections
+            .iter()
+            .map(|d| format!("    {}", d.xml()))
+            .collect::<Vec<String>>()
+            .join("\n");
+        format!(
+            "<particle nSpots='{}'>\n{}\n  </particle>",
+            self.detections.len(),
+            detections
+        )
+    }
 }
 
 #[derive(Debug)]
@@ -16,15 +53,19 @@ struct Detection {
     z: f64,
 }
 
-fn main() {
-    use calamine::{open_workbook, Reader, Xlsx};
+impl Detection {
+    fn xml(&self) -> String {
+        format!(
+            "<detection t='{}' x='{}' y='{}' z='{}' />",
+            self.t, self.x, self.y, self.z
+        )
+    }
+}
 
-    println!("opening");
+fn main() {
     let mut excel: Xlsx<_> = open_workbook("example.xlsx").unwrap();
-    println!("opened");
     let range = match excel.worksheet_range_at(0) {
         Some(Ok(r)) => {
-            println!("worksheet");
             match r
                 .rows()
                 .enumerate()
@@ -70,5 +111,6 @@ fn main() {
     let track = Track {
         particles: particles,
     };
-    println!("{:?}", track);
+    let header = "<?xml version='1.0' encoding='UTF-8'?>";
+    println!("{}\n{}", header, track.xml())
 }
